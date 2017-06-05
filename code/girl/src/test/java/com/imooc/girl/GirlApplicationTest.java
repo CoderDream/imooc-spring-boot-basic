676e06033135b6b09d514c84c8d995dd9f320833
CoderDream <coderdream@gmail.com>
2017-06-05 22:36:00 +0800

    init

---
+++ code/girl/src/test/java/com/imooc/girl/GirlApplicationTest.java
@@ -0,0 +1,18 @@
+package com.imooc.girl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Unit test for simple GirlApplication.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GirlApplicationTest {
+	@Test
+	public void contextLoads() {
+	}
+
+}
