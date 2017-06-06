21700647dd052abefba6867de89e8fd5b9162211
CoderDream <coderdream@gmail.com>
2017-06-06 20:45:10 +0800

    hello

--- code/girl/src/main/java/com/imooc/girl/HelloController.java
+++ code/girl/src/main/java/com/imooc/girl/HelloController.java
@@ -4,17 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class HelloController {
 	
 	@Autowired
 	private GirlProperties girlProperties;
 
-	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	@RequestMapping(value= {"/hello","/hi"}, method=RequestMethod.GET)
 	public String say(){
-		//return girlProperties.getCupSize();
-		return "index";
+		return girlProperties.getCupSize();
+		
 	}
 	
 }
