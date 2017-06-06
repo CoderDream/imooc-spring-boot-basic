45aaf491059e8d748190c6ef73b9901ef37ad8a9
CoderDream <coderdream@gmail.com>
2017-06-06 20:41:14 +0800

    update for thymeleaf

--- code/girl/src/main/java/com/imooc/girl/HelloController.java
+++ code/girl/src/main/java/com/imooc/girl/HelloController.java
@@ -1,33 +1,20 @@
 package com.imooc.girl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-	/*
-	@Value("${cupSize}")
-	private String cupSize;
-	
-	@Value("${age}")
-	private Integer age;
-	
-	@Value("${content}")
-	private String content;
-	*/
 	
 	@Autowired
 	private GirlProperties girlProperties;
 
 	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	public String say(){
-		//return "Hello Spring Boot!";
-		
-		//return cupSize + " " + age;
-		//return content;
-		return girlProperties.getCupSize();
+		//return girlProperties.getCupSize();
+		return "index";
 	}
 	
 }
