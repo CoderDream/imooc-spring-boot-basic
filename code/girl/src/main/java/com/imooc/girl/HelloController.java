e83386b8abd923eb44d30a78a5ccef15a0fca91f
CoderDream <coderdream@gmail.com>
2017-06-07 20:32:23 +0800

    controller

--- code/girl/src/main/java/com/imooc/girl/HelloController.java
+++ code/girl/src/main/java/com/imooc/girl/HelloController.java
@@ -1,8 +1,11 @@
 package com.imooc.girl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,12 +15,33 @@ public class HelloController {
 	private GirlProperties girlProperties;
 
 	/**
-	 * POST方式可以用SoapUI访问，不写的话GET/POST方式都可以访问
+	 * POST方式可以用SoapUI访问，不写的话GET/POST方式都可以
 	 * @return
 	 */
-	@RequestMapping(value= "/say")
-	public String say(){
-		return girlProperties.getCupSize();		
+	@RequestMapping(value= "/{id}/say", method=RequestMethod.GET)
+	public String say(@PathVariable("id") Integer id){
+		//return girlProperties.getCupSize();
+		return "id: " + id;
+	}
+	
+	@RequestMapping(value= "/say02", method=RequestMethod.GET)
+	public String say02(@RequestParam("id") Integer id){
+		//return girlProperties.getCupSize();
+		return "id: " + id;
+	}
+	
+
+	@RequestMapping(value= "/say03", method=RequestMethod.GET)
+	public String say03(@RequestParam(value="id", required=false, defaultValue="0") Integer id){
+		//return girlProperties.getCupSize();
+		return "id: " + id;
+	}
+	
+
+	@GetMapping(value= "/say04")
+	public String say04(@RequestParam(value="id", required=false, defaultValue="0") Integer id){
+		//return girlProperties.getCupSize();
+		return "id: " + id;
 	}
 	
 }
